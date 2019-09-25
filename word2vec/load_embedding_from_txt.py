9c9061670fac4b9a38d7ab5b9038765ed6b2968b
151250176 <151250176@smail.nju.edu.cn>
2019-09-25 10:22:02 +0800

    add count

--- word2vec/load_embedding_from_txt.py
+++ word2vec/load_embedding_from_txt.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append('/home/lcy/nlp/SecretProject')
 
 from constant.constant import EMBEDDING_SIZE
@@ -6,8 +7,8 @@ from constant.system_path import TENCENT_EMBEDDING_LOC, EMBEDDING_TXT, DICT_TXT
 import numpy as np
 import pickle
 
-
 if __name__ == '__main__':
+    count = 0
     dictionary = {}
     embedding = []
     with open(TENCENT_EMBEDDING_LOC, encoding='utf-8') as f:
@@ -15,6 +16,9 @@ if __name__ == '__main__':
         f.readline()
         line = f.readline()
         while line:
+            count += 1
+            if count % 1000 == 0:
+                print('cur: ' + count)
             row = line.split(' ')
             dictionary[row[0]] = len(dictionary)
             cur_embedding = []
