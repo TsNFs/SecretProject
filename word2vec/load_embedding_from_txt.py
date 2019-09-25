a7eba7253b0efb5ce07b58476ba32fe55039316a
151250176 <151250176@smail.nju.edu.cn>
2019-09-25 11:01:03 +0800

    to handle dirty in tencent.txt

--- word2vec/load_embedding_from_txt.py
+++ word2vec/load_embedding_from_txt.py
@@ -10,7 +10,7 @@ import pickle
 if __name__ == '__main__':
     count = 0
     dictionary = {}
-    embedding = []
+    final_embedding = []
     with open(TENCENT_EMBEDDING_LOC, encoding='utf-8') as f:
         # jump the first row
         f.readline()
@@ -20,14 +20,20 @@ if __name__ == '__main__':
             if count % 1000 == 0:
                 print('cur: ' + str(count))
             row = line.split(' ')
-            dictionary[row[0]] = len(dictionary)
             cur_embedding = []
-            for i in range(1, EMBEDDING_SIZE + 1):
-                cur_embedding.append(float(row[i]))
-            embedding.append(cur_embedding)
+            # for dirty in data
+            try:
+                for i in range(1, EMBEDDING_SIZE + 1):
+                    cur_embedding.append(float(row[i]))
+                dictionary[row[0]] = len(dictionary)
+                embedding = [cur_embedding]
+                if len(final_embedding) == 0:
+                    final_embedding = np.array(embedding)
+                else:
+                    final_embedding = np.row_stack((final_embedding, embedding))
+            except:
+                print('error at line : ' + str(count) + line)
             line = f.readline()
-
-    final_embedding = np.array(embedding)
 
     # dump the data
     f = open(EMBEDDING_TXT, 'wb')
